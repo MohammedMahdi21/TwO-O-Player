65d28766e5e5cc7900ef0ade41fb6d2644d87b01
Mohammed <MMD.Mahdi21@gmail.com>
2022-08-26 02:25:20 +0000

    Create generate_question function that will generate 2 random numbers and then ask the user what is num1 + num2

--- game.rb
+++ game.rb
@@ -2,8 +2,20 @@ class Game
   def initialize
     player_1 = "Player 1"
     player_2 = "Player 2"
+    @is_player1 = true
+    @is_player2 = false
   end
 
-  def start
-    # start logic here
+  def generate_question 
+    num1 = rand(20)
+    num2 = rand(20)
+    puts "#{player.name}: what is #{num1} + #{num2}"
+    answer = get.chomp.to_i
+
+    if (num1 + num2 == @answer)
+      puts "YES. You are correct"
+    else
+      puts "Seriously? No"
+      player.lives -= 1
+    end
 end
