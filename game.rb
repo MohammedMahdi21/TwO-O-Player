b7a7c0369c54189da7d55cc85ce5ce14cc26ed09
Mohammed <MMD.Mahdi21@gmail.com>
2022-08-26 02:44:43 +0000

    Create start_game function with the loop logic for the game

--- game.rb
+++ game.rb
@@ -1,12 +1,13 @@
 class Game
   def initialize
-    player_1 = "Player 1"
-    player_2 = "Player 2"
+    @player1 = Player1
+    @player2 = Player2
     @is_player1 = true
-    @is_player2 = false
+    @game_over = false
   end
 
-  def generate_question 
+  # Ask a question and check the answer
+  def generate_question(player) 
     num1 = rand(20)
     num2 = rand(20)
     puts "#{player.name}: what is #{num1} + #{num2}"
@@ -18,4 +19,24 @@ class Game
       puts "Seriously? No"
       player.lives -= 1
     end
+
+    if (player.lives == 0)
+      game_over = game_over(player)
+    end
+
+    is_player1 = !is_player1
+  end
+
+  #Create the loop logic for the questions
+  def start_game 
+    puts " --- Game Start Now --- "
+    while !game_over
+      if is_player1
+        self.generate_question(@player1)
+      else
+        self.generate_question(@player2)
+      end
+      puts "#{player1.name} #{player1.lives}/3 vs #{@player2.name} #{player2.lives}/3"
+    end
+  end
 end
